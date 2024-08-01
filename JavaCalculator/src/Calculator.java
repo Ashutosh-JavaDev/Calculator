d6652df4fa69583ecec56ed85454a3ad0eeee119
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-01 20:12:20 +0530

    Java Swing Calculator

--- JavaCalculator/src/Calculator.java
+++ JavaCalculator/src/Calculator.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame implements ActionListener {
     JButton[] numberbutton=new JButton[10];
-    JButton[] functionButtons=new JButton[7];
+    JButton[] functionButtons=new JButton[8];
     JPanel panel=new JPanel();
     JTextField text;
     Calculator() {
