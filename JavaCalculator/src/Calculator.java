c14216e8b47df1e7f1db2782e6f315f98395caf4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-01 20:08:45 +0530

    Java Swing Calculator

--- JavaCalculator/src/Calculator.java
+++ JavaCalculator/src/Calculator.java
@@ -3,11 +3,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Calculator extends JFrame implements ActionListener {
+    Calculator(){
+        
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
     @Override
     public void actionPerformed(ActionEvent ae){
-        
+
     }
 }
