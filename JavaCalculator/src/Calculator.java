97087bafcf233f67d4f87eeff5d8eb388f515086
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-01 21:06:17 +0530

    Calculator

--- JavaCalculator/src/Calculator.java
+++ JavaCalculator/src/Calculator.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-
+import java.awt.GridLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -8,29 +8,34 @@ import java.awt.event.ActionListener;
 public class Calculator extends JFrame implements ActionListener {
     JButton[] numberbutton = new JButton[10];
     JButton[] functionButtons = new JButton[8];
+    JButton add, sub, mul, div, clr, del, equal, point;
     JPanel panel;
     JTextField text;
     Font font = new Font("Calibri", Font.BOLD, 25);
     char operator;
+    double num1 = 0, num2 = 0, result;
+
     Calculator() {
         setTitle("Calculator");
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(450, 550);
-        setLocation(400,100);
+        setLocation(400, 100);
         functionoperation();
         setVisible(true);
     }
-    private void functionoperation(){
+
+    private void functionoperation() {
         // text
-        text=new JTextField();
+        text = new JTextField();
         text.setFont(font);
-        text.setBounds(10,10,430,50);
+        text.setEditable(false);
+        text.setBounds(10, 10, 430, 50);
         add(text);
-        //panel
-        panel=new JPanel();
-        panel.setBounds(10,80,430,350);
-        // panel.setBackground(Color.BLACK);
+        // panel
+        panel = new JPanel();
+        panel.setBounds(10, 80, 430, 350);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
         panel.setFont(font);
         add(panel);
 
