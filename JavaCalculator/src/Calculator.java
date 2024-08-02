ff46fd2471d3223dac2e38e2bba8e05a8e054a70
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-02 20:00:20 +0530

    Calculator

--- JavaCalculator/src/Calculator.java
+++ JavaCalculator/src/Calculator.java
@@ -64,9 +64,9 @@ public class Calculator extends JFrame implements ActionListener {
             functionButtons[i].setFont(font);
             functionButtons[i].setFocusable(false);
         }
-        // del.setBounds(15, 480, 200, 50);
-        // add(del);
-        clr.setBounds(80,480,300,50);
+        del.setBounds(15, 480, 200, 50);
+        add(del);
+        clr.setBounds(230,480,300,50);
         add(clr);
 
     }
